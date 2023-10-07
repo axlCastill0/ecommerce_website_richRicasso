d78f991a84c592c5442b7532cfe4128b2a0df3da
axlCastill0 <axl.castilloblasco@gmail.com>
2023-10-06 23:05:50 -0400

    formatted documents

--- index.php
+++ index.php
@@ -1,18 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rich Ricasso</title>
     <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="index.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
+
 <body>
     <?php include("header.php") ?>
     <main class="container">
-        <h1>hello</h1>
+        <div class="story">
+            <img src="img/rich-ricasso.png" alt="Rich Ricasso picture">
+            <p>Rich Ricasso, le célèbre cravatier et chemisier, souhaite lancer sa boutique en ligne pour présenter sa
+                nouvelle gamme d'été en soie unisexe. Son style, inspiré du mouvement vaporwave et vaporfashion accentue
+                les tons pastel tels que le bleu ciel, le rose poudré et le violet lavande, et a ainsi conquis le monde
+                de l'ultra-luxe. Il souhaite maintenant ouvrir sa gamme au grand public. Votre mission est de concevoir
+                et réaliser cette plateforme, mettant en avant la singularité et le luxe des produits.</p>
+        </div>
     </main>
     <?php include("footer.php") ?>
 </body>
+
 </html>
