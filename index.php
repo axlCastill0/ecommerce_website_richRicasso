78d5b94657f47820404992a8c8d18391b543a2d4
axlCastill0 <axl.castilloblasco@gmail.com>
2023-10-11 15:40:40 -0400

    MVC model and back to where left off

--- index.php
+++ index.php
@@ -4,17 +4,32 @@ include 'includes/class-autoload.inc.php';
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rich Ricasso</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/index.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
-<body>
-    <h1>hello</h1>
-    <?php
-        $productsObj = new ProductsView();
 
-        $productsObj->showUser("River in the desert - Rich Ricasso 1st Edition");
-    ?>
+<body>
+    <?php include("pages/headfoot/header.php") ?>
+    <div class="intro-image-container">
+        <img src="img/intro-shirt.png" alt="cool shirt" id="intro-img">
+    </div>
+    <main class="main-container">
+        <div class="popular-container">
+            <h2 class="popular-title">Popular</h2>
+            <div class="popular-cards">
+
+            </div>
+    </main>
+    <?php include("pages/headfoot/footer.php") ?>
 </body>
+
 </html>
---
+++ pages/headfoot/footer.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <footer class="footer">
+        <div class="footer-content">
+            <ul class="socials">
+                <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
+                <li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fa-brands fa-youtube"></i></a></li>
+                <li><a href="#"><i class="fa-brands fa-facebook"></i></a></li>
+            </ul>
+        </div>
+        <div class="footer-bottom">
+            <p>Copyright &copy; 2023 - Designed by <span>Les Tacos Rouges</span></p>
+        </div>
+    </footer>
+</body>
+
+</html>
