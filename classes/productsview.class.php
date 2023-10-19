334964aae76db7b74eb06c4a3fcbd9cae062dd88
axlCastill0 <axl.castilloblasco@gmail.com>
2023-10-19 11:14:58 -0400

    session in product window

--- classes/productsview.class.php
+++ classes/productsview.class.php
@@ -8,7 +8,7 @@ class ProductsView extends Products {
         $this->productsArray = $this->getProducts();
 
         foreach ($this->productsArray as $product) {
-            echo '<div class="product-card"><a href="/PHP/Projet02/pages/product.php">'
+            echo '<div class="product-card"><a href="/PHP/Projet02/pages/product.php?id='.$product['prod_id'].'">'
             . '<img src="/PHP/Projet02/img/productImages/' . $product['prod_id'] . '.png">'
             . '<div class="product-info"><h3 class="product-title">' . $product['prod_name'] . '</h3>'
             . '<h4 class="product-price">' . $product['prod_price'] . '</h4></div></a></div>';
---
+++ pages/product.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+if (isset($_GET["id"])) {
+    $id = $_GET["id"];
+} else {
+    header("location: /PHP/Projet02/");
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RR - Product</title>
+    <link rel="stylesheet" href="/PHP/Projet02/css/styles.css">
+    <link rel="stylesheet" href="/PHP/Projet02/css/shop.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+    <?php include("includes/header.php"); ?>
+    <main>
+        <h1><?php echo $id; ?></h1>
+    </main>
+    <?php include("includes/footer.php"); ?>
+</body>
+
+</html>
